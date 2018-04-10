8ca0b41f1d48b3686c718c633d294e7628fd8c2e
cym <bombcoder@outlook.com>
2018-04-10 14:35:16 +0800

    init: copy data struct from ngrok

---
+++ server/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"github.com/qiniu/log"
+	"sync"
+	"time"
+	"net"
+)
+
+type Options struct {
+	tunnelAddr string
+	domain     string
+}
+
+type ReqTunnel struct {
+	ReqId    string
+
+	// tcp only
+	RemotePort uint16
+}
+
+type Conn interface {
+	net.Conn
+	Id() string
+	SetType(string)
+	CloseRead() error
+}
+
+
+type Message interface{}
+
+type Shutdown struct {
+	sync.Mutex
+	inProgress bool
+	begin      chan int // closed when the shutdown begins
+	complete   chan int // closed when the shutdown completes
+}
+
+type Control struct {
+
+	// actual connection
+	conn Conn
+
+	// put a message in this channel to send it over
+	// conn to the client
+	out chan Message
+
+	// read from this channel to get the next message sent
+	// to us over conn by the client
+	in chan Message
+
+	// the last time we received a ping from the client - for heartbeats
+	lastPing time.Time
+
+	// all of the tunnels this control connection handles
+	tunnels []*Tunnel
+
+	// proxy connections
+	proxies chan Conn
+
+	// identifier
+	id string
+
+	// synchronizer for controlled shutdown of writer()
+	writerShutdown *Shutdown
+
+	// synchronizer for controlled shutdown of reader()
+	readerShutdown *Shutdown
+
+	// synchronizer for controlled shutdown of manager()
+	managerShutdown *Shutdown
+
+	// synchronizer for controller shutdown of entire Control
+	shutdown *Shutdown
+}
+
+
+type Tunnel struct {
+	// 隧道建立请求
+	req *ReqTunnel
+
+	// time when the tunnel was opened
+	start time.Time
+
+	// 公网 url
+	url string
+
+	// tcp 监听
+	listener *net.TCPListener
+
+	// 控制连接
+	ctl *Control
+
+	// closing
+	closing int32
+}
+
+type TunnelRegistry struct {
+	tunnels  map[string]*Tunnel
+	//affinity *cache.LRUCache
+	sync.RWMutex
+}
+
+func main(){
+	log.Print("start!")
+	//opts := &Options{
+	//	tunnelAddr:":4443",
+	//	domain:"127.0.0.1",
+	//}
+
+}
