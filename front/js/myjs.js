0e9c5279fe81464cd75528595e49c90b78e3a0d6
cym <bombcoder@outlook.com>
2018-05-11 00:44:19 +0800

    js:client js is ok

--- front/js/myjs.js
+++ front/js/myjs.js
@@ -1,3 +1,37 @@
+console.log($("#clients>tr>td#client-id").text())
+
+
+
+function appendClient(key) {
+    var clientId;
+    clientId=arrayCtl[key];
+    str = "  <tr id='client' new=true>\n" +
+        "                <td id='client-id'>"+clientId+
+        "</td>\n" +
+        "                <td id='tunnel-number'>2</td>\n" +
+        "                <td>\n" +
+        "                    <div class='dropdown line-div'>\n" +
+        "                        <button class='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1'\n" +
+        "                                data-toggle='dropdown'>\n" +
+        "                            操作\n" +
+        "                            <span class='caret'></span>\n" +
+        "                        </button>\n" +
+        "                        <ul class='dropdown-menu' aria-labelledby='dropdownMenu1'>\n" +
+        "                            <li><a href='#'>关闭控制连接</a></li>\n" +
+        "                            <li role='presentation'><a href='' role='button' data-toggle='modal' data-target='#myModal'>查看隧道信息</a>\n" +
+        "                            </li>\n" +
+        "                        </ul>\n" +
+        "                    </div>\n" +
+        "                </td>\n" +
+        "                <td>\n" +
+        "                    <span class='light red' id='client-status'></span>\n" +
+        "                </td>\n" +
+        "            </tr>"
+
+    $("#clients").append(str)
+}
+
+
 $(document).ready(function () {
 
     $("span.glyphicon-off").click(function () {
@@ -15,16 +49,15 @@ $(document).ready(function () {
 //     Consnum    int
 // }
 
-// 开启服务
-//ajax1 = $.getJSON("/status-on", function (r) {
-//})
 
 getInfo();
 
 
-setInterval(function() {
-   getInfo();
+setInterval(function () {
+    getInfo();
 }, 2000);
+
+Ctls=new Array();
 
 // 更新服务器状态
 function getInfo() {
@@ -32,21 +65,28 @@ function getInfo() {
 
         if (r['Status'] == true) {
             $("span#server-status").attr('class', 'light green');
-            $("span#server-status").attr('status',1);
+            $("span#server-status").attr('status', 1);
             $("a#open-server").text("关闭服务器");
         } else {
             $("span#server-status").attr('class', 'light red');
-            $("span#server-status").attr('status',0);
+            $("span#server-status").attr('status', 0);
             $("a#open-server").text("打开服务器");
         }
 
         // 更新客户端信息
         arrayCtl = r['Ctls'];
-        // console.log(arrayCtl);
+
         if (arrayCtl != null) {
             for (var key in  arrayCtl) {
-                console.log(key);
-                $("td#client-id").text(key);
+                i=Ctls.indexOf(arrayCtl[key])
+                // console.log(i);
+                if (i==-1){
+                    Ctls.push(arrayCtl[key])
+                    appendClient(key)
+                }else{
+
+                }
+                ;
             }
         }
 
@@ -54,22 +94,22 @@ function getInfo() {
 }
 
 
-
 // 开关服务器
 function changeServer() {
-    status=$("span#server-status").attr("status");
+    status = $("span#server-status").attr("status");
     console.log(status);
-    if(status==0){
+    if (status == 0) {
         $.getJSON("/status-on", function (r) {
             alert("on")
         })
-    }else{
+    } else {
         $.getJSON("/status-off", function (r) {
             alert("off")
         })
     }
 
 }
+
 // 监听开关服务器器
 $(document).ready(function () {
         $("a#open-server").click(function () {
